8a0e0050eb8ff7bbacde439409fc7de561b5a0ea
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-09-15 10:51:22 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".nav-toggle");
+  const menu = document.querySelector(".nav-menu");
+  toggle.addEventListener("click", () => menu.classList.toggle("open"));
+});
