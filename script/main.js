3049bd1390cdcbee5fe5323732ebfa69ad742404
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-09-15 10:48:34 +0700

    Delete script directory

--- script/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".nav-toggle");
-  const menu = document.querySelector(".nav-menu");
-  toggle.addEventListener("click", () => menu.classList.toggle("open"));
-});
